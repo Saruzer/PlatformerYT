17d0e5b3345367406221b9a6ed842e35045ca4b4
Saruzer Saruzer <mrlexa130@gmail.com>
2025-05-07 08:21:32 +0300

    Урок 6 Відображення рахунку

--- Assets/Scripts/PlayerData.cs
+++ Assets/Scripts/PlayerData.cs
@@ -1,12 +1,20 @@
+using TMPro;
+
 using UnityEngine;
 
 public class PlayerData : MonoBehaviour
 {
     [SerializeField] private int _score;
+    [SerializeField] private TextMeshProUGUI _scoreText;
 
+    private void Start()
+    {
+        UpdateScoreText();
+    }
     public void AddScore(int amount) 
     {
         _score += amount;
+        UpdateScoreText();
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -15,5 +23,9 @@ public class PlayerData : MonoBehaviour
         {
             AddScore(coin.Collect());
         }
+    }
+    private void UpdateScoreText() 
+    {
+        _scoreText.text = "Рахунок: " + _score.ToString();
     }
 }
