d6efadfba81ea6f913adcf0911c6e684199b6696
Saruzer Saruzer <mrlexa130@gmail.com>
2025-05-07 08:52:11 +0300

    Урок 7 Індикатор здоров'я

--- Assets/Scripts/PlayerData.cs
+++ Assets/Scripts/PlayerData.cs
@@ -1,14 +1,19 @@
 using TMPro;
 
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerData : MonoBehaviour
 {
     [SerializeField] private int _score;
+    [SerializeField] private int _health;
+    [SerializeField] private int _maxHelath;
     [SerializeField] private TextMeshProUGUI _scoreText;
+    [SerializeField] private Image _healthBar;
 
     private void Start()
     {
+        UpdateHealthBar();
         UpdateScoreText();
     }
     public void AddScore(int amount) 
@@ -27,5 +32,9 @@ public class PlayerData : MonoBehaviour
     private void UpdateScoreText() 
     {
         _scoreText.text = "Рахунок: " + _score.ToString();
+    }
+    private void UpdateHealthBar() 
+    {
+        _healthBar.fillAmount = (float)_health / (float)_maxHelath;
     }
 }
